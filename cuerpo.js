839991609de05bd4882048719506557b862fee8e
¨brahiam10¨ <brahianamarin23@gmail.com>
2022-10-28 15:10:22 -0500

    Se crea el sitio web y se crea el header, menu con estilos

---
+++ cuerpo.js
@@ -0,0 +1,31 @@
+/*const contenedorOpciones = document.getElementById("contenedor-articulos")
+
+let opcioness = []
+let inputBlog
+let OpcionLista
+class option{
+    constructor(titulo ,texto){
+        this.titulo = titulo
+        this.texto=texto
+    }
+}
+
+let blog = new option("El mundo está lleno de experiencias, personas y lugares maravillosos" , "Sin embargo, millones de personas se conforman con sobrevivir. Aceptan las reglas impuestas por la sociedad, llevan una vida “cómoda” y “segura” (pero bastante aburrida), y renuncian a lo que de verdad les hace sentirse vivos \n En el fondo de su corazón saben que ese no es el camino correcto. Que deben hacer algo al respecto. Que deben cambiar. Pero tienen miedo: \nMiedo a fracasar y perder lo que ya tienen Miedo a decepcionar a su familiaMiedo a ir a contracorriente y que los demás les juzguenY mientras tanto, pasan los días, las semanas, los años, y todo sigue igual.Lo sé porque mi vida fue así hace tiempo. Yo también estuve atrapado en un empleo de 9 a 6 que no me hacía feliz. Por suerte, reuní el valor necesario, descubrí la manera de escapar y ahora soy un hombre libre.")
+
+opcioness.push(blog);
+
+function opciones(){
+    inputBlog = document.getElementById("blog")
+}
+
+function condiciona()
+{
+    opcioness.forEach((eleccion)=>{
+        if(inputBlog.checked)
+        {
+            opcioness = ` <p>${eleccion.texto}</p>`
+            contenedorOpciones.innerHTML = opcioness
+        }
+        inputBlog = document.getElementById("blog");
+    })
+}*/
